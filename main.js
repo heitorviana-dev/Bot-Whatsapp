48e6b68a0887ecfdfb076e5f247b30befb054d89
heitorviana-dev <heitorsoaresviana@gmail.com>
2024-04-06 10:40:51 -0300

    Documentando main.js

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require("qrcode-terminal");
 
-//Classe que cria uma instância do cliente, sendo esse o starting point para a interação com a API Whatsapp Web
+// Classe que cria uma instância do cliente, sendo esse o starting point para a interação com a API Whatsapp Web.
+// Basicamente ao criarmos essa instância estamos criando uma janela no navegador programada com js, como se estivessemos abrindo o whatsapp web no navegador.
 const client = new Client({
-    webVersionCache: {
+    webVersionCache: { // Essa opção foi adicionada para evitar um erro no LocalCache
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
         type: 'remote'
     } 
 }); 
 
-// When the client is ready, run this code (only once) O cliente só estará preparado após ler o qrcode
+// Quando o cliente ler o qrcode será emitido o sinal "ready" e o código abaixo será executado.
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-// When the client received QR-Code
+// Ao criarmos a instância o evento qr será emitido, o código abaixo será responsável por capturá-lo.
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true}); // qrcode.generate(<conteúdo do qrcode>, <options>);
 });
 
-// Start your client
+// Inicia o cliente e permite a interação com o whatsapp web permitindo enviar e receber mensagens, e outras operações disponíveis na api.
 client.initialize();
